fcb5caf7e6f4c5c58e072e8553fb2b938f75d6ff
ahuounan <ahuounan@users.noreply.github.com>
2018-05-12 18:01:26 +0800

    Building OO HexWave
    - Completed lightning function
    - Built out event handling mechanism for Circles
    - Built out origin select mode sequence for grid

--- HexWave.js
+++ HexWave.js
@@ -1,35 +1,44 @@
 class Game {
 	constructor() {
 		this.speed = 1;
-	}	
+	}
+
+	toCircle(dom) {
+		let id = dom.getAttribute("id");
+		let re = (id, i) => {
+			return Number(/#(\d)i(\d\d\d)(\d\d\d)/.exec(id)[i]);
+		}
+		let grid = re(id, 1);
+		let row = re(id, 2);
+		let col = re(id, 3);
+		return this.grids[grid - 1].getCircle([row, col]);
+	}
 }
 
-Game.prototype.handlers = {
-			start: function(e) {
-				let circle = game.grid.toCircle(this)
-				if (e.type != "click") {
-					this.classList.toggle("select");
-					propagate( (n) => {draw(getHexA(circle.coords, n), "select", "toggle")}, 2, 0);
-				} else {
-					this.classList.toggle("base");
-					propagate( (n) => {draw(getHexA(circle.coords, n), "base", "toggle")}, 2, 0);
-					startGame();
-				}
-			}
-		}
 
 class HexGrid {
 	constructor(game, gridRadius) {
 		this.game = game;
 		this.gridRadius = gridRadius;
 		this.grid = [];
+		this.origin;
+		this.base = []
+		this.mode;
+
+		// Object that maps blast direction to pre-generated lists of circle rings.
+		// Speeds up blast animation in-game.
+		this.blastDir;
 
 		this.setGrid();
 		this.createHtml();
 		this.setCSSColors();
 		this.setCSSDimensions();
+		this.selectMode();
 	}
 
+	// Generate grid of circles, gridRadius x gridRadius
+	// Even rows numbering: 0, 2, 4, etc
+	// Odd rows numbering: 1, 3, 5, etc
 	setGrid() {
 		for (let y = 0; y < this.gridRadius; y++) {
 			let newRow = [];
@@ -41,14 +50,13 @@ class HexGrid {
 		}
 	}
 
+	// Generates HTML for circle grid
 	createHtml() {
 		let hexContainer = document.createElement("div");
-		hexContainer.classList.add("hex-container");
-		document.body.appendChild(hexContainer);
-
+		
 		let rowCount = 0
 		for (let row of this.grid) {
-			var curLine = document.createElement("div");
+			let curLine = document.createElement("div");
 			curLine.classList.add("row");
 			let rowType = (rowCount % 2 == 0) ? "even" : "odd";
 			curLine.classList.add(rowType);
@@ -59,8 +67,12 @@ class HexGrid {
 				curLine.appendChild(circle.dom);
 			}
 		}
+		hexContainer.classList.add("hex-container");
+		document.body.appendChild(hexContainer);
 	}
 
+	// Shuffles HTML grid colors by generaing randomg RGB colors
+	// and sends to CSS variables
 	setCSSColors() {
 		for (let color of ["r", "g", "b"]) {
 			let random = String(Math.floor(Math.random() * 155));
@@ -68,53 +80,40 @@ class HexGrid {
 		}
 	}
 
+	// Sets HTML grid circle size depending on window size
 	setCSSDimensions() {
 		let smallest = Math.min(window.innerWidth, window.innerHeight);
-		let total_dim = 10 > (smallest / this.griRadius) ? 10 : (smallest / this.gridRadius);
+		let total_dim = 5 > (smallest / this.gridRadius) ? 5 : (smallest / this.gridRadius);
 		document.documentElement.style.setProperty("--total_dim", total_dim + "px");
+		document.documentElement.style.setProperty("--gridRadius", (this.gridRadius * total_dim) + "px");
 	}
 
+	// Returns Circle object at coordinates row, col in grid
 	getCircle([row, col]) {
 		let colAdj = (row % 2) == 0 ? 0 : 1;
 		if (this.grid[row] == undefined) return undefined;
 		return this.grid[row][(col - colAdj) / 2];
 	}
 
-	toCircle(dom) {
-		let id = dom.getAttribute("id");
-		let re = (id, i) => {
-			return Number(/#i(\d\d\d)(\d\d\d)/.exec(id)[i]);
-		}
-		let row = re(id, 1);
-		let col = re(id, 2);
-		return this.getCircle([row, col]);
-	}
-
-	setOrigin() {
-		let events = ["mouseenter", "mouseleave", "click"];
-		for (let circle of this.grid) {
-			for (let e of events) {
-				circle.dom.addEventListener(e, this.game.handlers.start);
-			}
-		}
-	}
-
+	// Generates ring or partial ring based on origin circle
 	getRing([aRowDir, aColDir], rad, dir, seg, origin=this.origin) {
 		//aRow, aCol are direction of anchor line. They can be 1 or -1
 		//rad is current radius of ring
 		//dir is 1 for clockwise and -1 for counterclockwise
 		//seg is number of sextants to draw
-		console.log(seg);
 		let oCrds = origin.coords;
+
+		// Current coordinate, begins at origin + radius/direction
 		let cCrds = [oCrds[0] + aRowDir * (rad - 1), oCrds[1] + aColDir * (rad - 1)];
+
+		// Declare variables needed in calculations
 		let offset, y, x, s, z;
+
 		let result = [];
-		//Figure out which way it should move on anchor lines
-		for (let i = 0; i <= seg * (rad - 1); i++) {
+		for (let i = 0; i <= seg * (rad - 1) - 1; i++) {
 			let circle = this.getCircle(cCrds);
 			if (circle != undefined) {
-				result.push(circle)
-				circle.dom.style.background = "red";
+				result.push(circle);
 			}
 
 			offset = [cCrds[0] - oCrds[0], cCrds[1] - oCrds[1]];
@@ -133,6 +132,76 @@ class HexGrid {
 		}
 		return result;
 	}
+
+	// Takes list of circles and renders and colors/animations by toggling CSS classes
+	draw(list, type, cls) {
+		for (let circle of list) {
+			circle.dom.classList[type](cls);
+		}
+	}
+
+	// Grid enters set origin state, once origin is selected, starts the game
+	selectMode() {
+		this.mode = "select";
+		let events = ["mouseenter", "mouseleave", "click"];
+		let toggleEvents = (act) => {
+			for (let row of this.grid) {
+				for (let circle of row) {
+					for (let e of events) {
+						circle[act](e);
+					}
+				}
+			}
+		};
+		let action = (this.origin === undefined) ? 'addEvents' : 'removeEvents';
+		toggleEvents(action);
+		if (this.origin !== undefined) this.startGame();
+	}
+
+	setOrigin(target) {
+		this.mode = "game";
+		this.origin = target;
+		for (let rad = 2; rad < 4; rad++) {
+			this.base.push(this.getRing([1, 1], rad, 1, 6, this.origin));
+		}
+		this.startGame();
+	}
+
+	// Grid enters start game state.
+	startGame() {
+		this.blastDir = this.loadBlasts();
+		let events = ["mouseenter", "mouseleave", "click", "dblclick", "animationend"];
+		for (let row of this.grid) {
+			for (let circle of row) {
+				for (let e of events) {
+					circle.addEvents(e);
+				}
+			}
+		}
+	}
+
+	loadBlasts() {
+		// Preload blast lists to increase speed in game
+		let aRowDir = [1, 0, -1];
+		let aColDir = [1, -1];
+		let dir = [1, -1];
+		let seg = [1, 2, 3, 4, 5, 6];
+		let blastDir = {}
+
+		for (let r of aRowDir) {
+			for (let c of aColDir) {
+				for (let d of dir) {
+					for (let s of seg) {
+						blastDir[[r, c, d, s]] = []
+						for (let rad = 2; rad < grid.gridRadius; rad++) {
+							blastDir[[r, c, d, s]].push(this.getRing([r, c], rad, d, s, this.origin));
+						}
+					}
+				}
+			}
+		}
+		return blastDir;
+	}
 }
 
 class Circle {
@@ -143,6 +212,7 @@ class Circle {
 		this.dom = this.createDOM();
 	}
 
+	// Creates HTML object corresponding to circle
 	createDOM() {
 		let newCircle = document.createElement("div");
 
@@ -152,6 +222,7 @@ class Circle {
 		return newCircle;
 	}
 
+	// Creates ID based on circle coordinates
 	get id() {
 		let row_id = String(this.row).padStart(3,0);
 		let col_id = String(this.col).padStart(3,0);
@@ -163,172 +234,101 @@ class Circle {
 		return [this.row, this.col];
 	}
 
-	location(origin) {
-		let offset = origin.offset()
-		let yLoc = Math.abs(this.row - origin.row) / (this.row - origin.row) || 0;
-		let xLoc = Math.abs(this.col - origin.col) / (this.col - origin.col) || 1;
-		let slope = Math.abs(this.row) / Math.abs(this.col)
-
-		return {y: yLoc, x: xLoc, s: slope}
-	}
-
+	// Calculates row/col offset from target circle
 	offset(target) {
 		return [target.coords[0] - this.coords[0], target.coords[1] - this.coords[1]];
 	}
 
-	radius(target) {
-		let offset = this.offset(target);
-		return (Math.abs(offset[0]) + Math.abs(offset[1]) / 2);
-	}
-
+	// Calculates straight-line distnace from target circle
 	distance(target) {
 		let offset = this.offset(target);
-		return (Math.abs(offset[0]) + Math.abs(offset[1]) / 2);
+		return Math.sqrt((offset[0] ** 2 + (offset[1] / 2) ** 2));
 	}
-
-	move([row, col]) {
-		col *= (row == 0) ? 2 : 1;
-		return this.grid.getCircle([this.row + row, this.col + col]);
-	}
-}
-
-
-function getRadius(origin, coords) {
-	let offset = getOffset(origin, coords);
-	return (Math.abs(offset[0]) + Math.abs(offset[1]) / 2);
-}
-
-function getDistance(origin, coords) {
-	let offset = getOffset(origin, coords);
-	return Math.sqrt(((offset[0]) ** 2+ ((offset[1]) / 2) ** 2));
-}
-
-function getLine(origin, coords, line=[]) {
-	let offset = getOffset(origin, coords);
-	let row_dir = (offset[0] / Math.abs(offset[0]) || 0);
-	let col_dir = (offset[1] / Math.abs(offset[1]) || 1);
-	let moves = [
-		[origin[0] + 0, origin[1] + 2 * col_dir], //horizontal
-		[origin[0] + row_dir, origin[1] + col_dir], //diagonal
-		[origin[0] - row_dir, origin[1] + col_dir],
-		[origin[0] + row_dir, origin[1] - col_dir]
-	]
 	
-	let next = moves.reduce((acc, curr) => {
-		return getDistance(acc, coords) > getDistance(curr, coords) ? curr : acc
-	});
-	if (origin[0] != coords[0] || origin[1] != coords[1]) {
-		line.push(next);
-		return getLine(next, coords, line);
-	} else {
-		return line;
+	// Draws a straight-line from this circle to target. 
+	lightning(target) {
+		this.draw("toggle", "lightning");
+
+		let moves = this.grid.getRing([1, 1], 2, 1, 6, this);
+		let next = moves.reduce((acc, curr) => {
+			return acc.distance(target) > curr.distance(target) ? curr : acc;
+		});
+
+		if (this.coords[0] != target.coords[0] || this.coords[1] != target.coords[1]) {
+			next.lightning(target);
+		}
 	}
-}
 
-
-function propagate(fn, max, delay, n=0) {
-	if (n < max) {
-		n++;
-		setTimeout(() => {
-			fn(n);
-			propagate(fn, max, delay, n)
-		}, delay);
+	draw(type, cls) {
+		this.dom.classList[type](cls);
 	}
-}
 
-function draw(coords, effect, type) {
-	for (c of coords) {
-		let circle = document.querySelector(getID(c));
-		if (circle != undefined) {
-			if (type == "toggle") {
-				circle.classList.toggle(effect);
-			} else if (type == "add") {
-				circle.classList.add(effect);
-			} else {
-				circle.classList.remove(effect);
+	// Adds event handler to circle
+	addEvents(type) {
+		this.dom.addEventListener(type, this);
+	}
+
+	// Removes event handler to circle
+	removeEvents(type) {
+		this.dom.removeEventListener(type, this);
+	}
+
+	// Switchboard for event handlers
+	handleEvent(e) {
+		if(this.grid.mode == "select") {
+			this.selectHandler(e);
+		} else if (this.grid.mode == "game") {
+			switch(e.type) {
+				case 'click':
+					this.clickHandler(e);
+					break
+				case 'dblclick':
+					this.dblClickHandler(e);
+					break
+				case 'mouseenter':
+					this.enterHandler(e);
+					break
+				case 'mouseleave':
+					this.leaveHandler(e);
+					break
+				case 'animationend':
+					this.clearAnimation(e);
+					break
 			}
 		}
 	}
-}
 
-function dblClickHandler() {
-	event.preventDefault()
-	propagate((n) => { draw(getHex(base, getCoords(this), n), "wave", "add") }, 100, 1)
-	// draw(getLine(base,getCoords(this)), "lightning", "add")
-}
+	selectHandler(e) {
+		let cls = e.type == "click" ? "base" : "select"
+		this.dom.classList.toggle(cls);
+		for (let i = 2; i <=3; i++) {
+			this.grid.draw(this.grid.getRing([1, 1], i, 1, 6, this), "toggle", cls);
+		}
 
-function clickHandler() {
-	draw(getLine(base,getCoords(this)), "lightning", "toggle");
-}
+		if (e.type == "click") this.grid.setOrigin(this);
+	}
 
-function mouseHandler() {
-	draw(getLine(base,getCoords(this)), "route", "toggle");
-}
+	clickHandler(e) {
+		this.grid.origin.lightning(this);
+	}
 
-function baseHandler() {
-	shuffleColors();
-	propagate((n) => { draw(getHexA(base, n), "wave", "add") }, 100, 1)
-}
+	dblClickHandler(e) {
 
-function clearAnimation() {
-	this.classList.remove("wave");
-	this.classList.remove("lightning");
-}
+	}
 
-function redraw() {
-	document.body.innerHTML = "";
-	generateGrid();
-	selectOrigin();
-}
+	enterHandler(e) {
 
-function startHandler(e) {
-	if (e.type != "click") {
-		this.classList.toggle("select");
-		propagate( (n) => {draw(getHexA(getCoords(this), n), "select", "toggle")}, 2, 0);
-	} else {
-		base = getCoords(this);
-		this.classList.toggle("base");
-		propagate( (n) => {draw(getHexA(getCoords(this), n), "base", "toggle")}, 2, 0);
-		startGame();
+	}
+
+	leaveHandler(e) {
+
+	}
+
+	clearAnimation(e) {
+		this.dom.classList.remove("lightning");
 	}
 }
 
-function selectOrigin() {
-	circles = document.querySelectorAll(".circle");
-	for (c of circles) {
-		c.addEventListener("mouseenter", startHandler);
-		c.addEventListener("mouseleave", startHandler);
-		c.addEventListener("click", startHandler);
-	}
-}
-
-function startGame () {
-	circles = document.querySelectorAll(".circle");
-	for (c of circles) {
-			c.removeEventListener("mouseenter", startHandler);
-			c.removeEventListener("mouseleave", startHandler);
-			c.removeEventListener("click", startHandler);
-
-			if (!c.classList.contains("select")) {
-				c.addEventListener("click", clickHandler);
-				c.addEventListener("animationend", clearAnimation);
-				c.addEventListener("dblclick", dblClickHandler);
-				c.addEventListener("mouseenter", mouseHandler);
-				c.addEventListener("mouseleave", mouseHandler);
-			}
-	}
-	bases = document.querySelectorAll(".select");
-	for (b of bases) {
-		b.addEventListener("click", baseHandler);
-	}
-	shuffleColors();
-}
-
-function main() {
-	redraw();
-	selectOrigin();
-	window.addEventListener("resize",redraw);
-}
-
-let base;
-// main();
+let game = new Game();
+let grid = new HexGrid(game, 50);
+grid.selectMode();
